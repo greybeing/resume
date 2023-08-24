76f894ad1b997e7a5296784a74d5e69ae9aa24a3
greybeing <adulojugbenga@yahoo.com>
2023-08-24 18:30:54 +0100

    site counter

--- js/counter.js
+++ js/counter.js
@@ -1,23 +1,34 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    getVisitCount();
-});
+// window.addEventListener('DOMContentLoaded', (event) => {
+//      getVisitCount();
+   
+// });
 
 
-const funcApi = 'https://resumevisitcounter.azurewebsites.net/api/counterfunction?code=jgAeSzqhStpwaf6yyAVZ24FD6N/ZanUYVDuf4p23huABl9ftAUnHQg==';
+// const funcApi = 'https://resumevisitcounter.azurewebsites.net/api/counterfunction?code=jgAeSzqhStpwaf6yyAVZ24FD6N/ZanUYVDuf4p23huABl9ftAUnHQg==';
 
 
-const getVisitCount = () => {
-    let count = 0;
-    fetch(funcApi)
-    .then(response => {
-        return response.json()
-    })
-    .then(response => {
-        console.log("Website called function API.");
-        count = response.count;
-        document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
-        console.log(error);
-      });
-    return count;
+// const getVisitCount = () => {
+//     let count = 0;
+//     fetch(funcApi)
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(response => {
+//         console.log("Website called function API.");
+//         count = response.count;
+//         document.getElementById('counter').innerText = count;
+//     }).catch(function(error) {
+//         console.log(error);
+//       });
+//     return count;
+// }
+
+ 
+async function fetchVisitorCount() {
+    const response = await fetch('http://localhost:9100/visitors/');
+    const data = await response.json();
+    document.getElementById('counter').innerText = data.count;
 }
+
+fetchVisitorCount();
+
