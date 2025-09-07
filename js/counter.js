af4ae6b48f686ce6212a61ef9ccd4b5f99a1b96e
greybeing <adulojugbenga@yahoo.com>
2025-09-07 16:01:15 +0100

    counter function

--- js/counter.js
+++ js/counter.js
@@ -1,4 +1,4 @@
- 
+// counter function to fetch visitor count from API and display it in the HTML element with id 'counter'
 async function fetchVisitorCount() {
     const response = await fetch('https://api.oluwagbenga.com/visitors/');
     const data = await response.json();
